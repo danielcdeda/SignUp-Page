93fa919740dbf611c6ab7d4f476b1106e44e5491
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-04-24 18:02:16 -0300

    adicionado post request

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const https = require('https');
 const bodyParser = require('body-parser');
 const request = require('request');
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/signup.html")
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+    const firstN = req.body.firstN
+    const secondN = req.body.secondN
+    const email = req.body.email
+
+    console.log(firstN, secondN, email);
 })
 
 
 app.listen(2048, function(){
-    console.log("Working on port 2048!")
-})
+    console.log("Working on port 2048!");
+});
