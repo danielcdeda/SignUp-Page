b3e736b4c06cc8fe85a085d9acfa2de196ff537c
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-04-24 14:48:40 -0300

    sign up page

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
