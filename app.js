c5384c9d8b75eca30eaa5b62747f6dbec84c0345
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-04-24 16:03:14 -0300

    changed directories

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const request = require('request');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html")
+})
 
 
 app.listen(2048, function(){
