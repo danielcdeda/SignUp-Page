5c3f7568c7d53873ae3c2a1ff155aa50865d4fff
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-04-25 16:10:38 -0300

    .

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const client = require("@mailchimp/mailchimp_marketing");
 
 const app = express();
+client.setConfig({apiKey: "4b80286443a2b71afffe58a667e7e30e-us14",  server: "us14",});
 
 app.use(express.static(__dirname + '/'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,11 +17,38 @@ app.post("/", function(req, res){
     const firstN = req.body.firstN
     const secondN = req.body.secondN
     const email = req.body.email
+    const subscribingUser = {firstName: firstName, lastName: lastName, email: email}
 
-    console.log(firstN, secondN, email);
+    const run = async () => {
+        const response = await client.lists.addListMember("4b80286443a2b71afffe58a667e7e30e-us14", {
+          email_address: subscribingUser.email,
+          status: "subscribed",
+          merge_fields: {
+              FNAME: subscribingUser.firstName,
+              LNAME: subscribingUser.lastName
+          }
+        });
+        console.log(response);
+      };
 })
 
+
+var data = {
+    members: [
+        {
+            
+        }
+    ]
+}
 
 app.listen(2048, function(){
     console.log("Working on port 2048!");
 });
+
+
+
+// API Key
+// 4b80286443a2b71afffe58a667e7e30e-us14
+
+// LIST ID 
+// 10f1fc523e
